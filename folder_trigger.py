1569918250daffead5ce12fd04c5c7a862d50430
Patrick <paddiolsen@gmail.com>
2021-08-04 13:21:08 +0200

    setup a event handler for created, deleted, modified, moved started the observer and made i exit with keyboardinterrupt.

--- folder_trigger.py
+++ folder_trigger.py
@@ -2,6 +2,18 @@ import time
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
+def on_created(event):
+    print(f"Hey, {event.src_path} has been created!")
+
+def on_deleted(event):
+    print(f"Someone has deleted {event.src_path}")
+
+def on_modified(event):
+    print(f"hey buddy, {event.src_path} has been modified")
+
+def on_moved(event):
+    print(f"Ok someone moved {event.src_path} to {event.dest_path}")
+
 
 
 
@@ -11,3 +23,20 @@ if __name__ == "__main__":
     ignore_directories = False
     case_sensitive = True
     my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+    my_event_handler.on_created = on_created
+    my_event_handler.on_deleted = on_deleted
+    my_event_handler.on_modified
+    my_event_handler.on_moved
+
+    path = "."
+    go_recursively = True
+    my_observer = Observer()
+    my_observer.schedule(my_event_handler, path, recursive=go_recursively)
+
+    my_observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        my_observer.stop()
+        my_observer.join()
